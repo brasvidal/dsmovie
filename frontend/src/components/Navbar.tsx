4b2488ad7fca33b6352cda2ad1e91aa1feed78c5
Marcos Vidal <marcosvidal@outlook.com>
2022-05-09 18:57:40 -0300

    Refatoração Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+import { ReactComponent as GitHubIcon } from '../assets/img/github.svg'
+
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/brasvidal" rel='nooppener noreferrer'  >
+                        <div className="dsmovie-contact-container">
+                            <GitHubIcon />
+                            <p className="dsmovie-contact-link">/brasvidal</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header >
+    );
+}
+
+export default Navbar;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import Navbar from 'components/Navbar';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>
 );
