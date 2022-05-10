f28605730d5d0eb2a8419db8d164f93e75d87dc6
Marcos Vidal <marcosvidal@outlook.com>
2022-05-10 09:22:10 -0300

    Added linkedin at navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import { ReactComponent as GitHubIcon } from 'assets/img/github.svg'
+import { ReactComponent as LinkedIn } from 'assets/img/linkedin-logo.svg'
 
 
 function Navbar() {
@@ -8,12 +9,22 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/brasvidal" rel='nooppener noreferrer'  >
-                        <div className="dsmovie-contact-container">
-                            <GitHubIcon />
-                            <p className="dsmovie-contact-link">/brasvidal</p>
-                        </div>
-                    </a>
+                    <div className="dsmovie-social-container">
+                        <a href="https://linkedin.com/in/mvidaldev" target='_blank' rel='nooppener noreferrer'  >
+                            <div className="dsmovie-contact-container">
+                                <LinkedIn />
+                                <p className="dsmovie-contact-link">/mvidaldev</p>
+                            </div>
+                        </a>
+                        <a href="https://github.com/mvidaldev" target='_blank' rel='nooppener noreferrer'  >
+                            <div className="dsmovie-contact-container">
+                                <GitHubIcon />
+                                <p className="dsmovie-contact-link">/mvidaldev</p>
+                            </div>
+                        </a>
+                    </div>
+
+
                 </div>
             </nav>
         </header >
